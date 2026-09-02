124a5e67c945cafcf9599ba198e9ac6902ce8402
Iammdtanvirrahman2007 <mdtanvirrahman2021a@gmail.com>
2026-09-02 19:03:29 +0600

    Add Google Drive OAuth configuration

---
+++ js/drive-config.js
@@ -0,0 +1,5 @@
+// Google Drive configuration for Tanmira Chat.
+// Create a Web application OAuth 2.0 Client ID in Google Cloud and paste it below.
+// Authorized JavaScript origin: https://iammdtanvirrahman2007.github.io
+export const GOOGLE_CLIENT_ID = 'PASTE_YOUR_WEB_CLIENT_ID.apps.googleusercontent.com';
+export const DRIVE_SCOPE = 'https://www.googleapis.com/auth/drive.file';
